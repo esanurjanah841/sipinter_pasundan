c3fecccf8c15595d62c466440988cb67d6889862
esanurjanah841 <nurjanahesa@gmail.com>
2024-03-20 21:43:03 +0700

    123456

--- register.php
+++ register.php
@@ -30,6 +30,8 @@ if (isset($_SESSION['role'])) {
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 
+<link href="assets/css/style.css" rel="stylesheet">
+
 <title>Si Pinter</title>
 </head>
 
